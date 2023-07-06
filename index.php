5cd9065fe4211e15b8d83d1db61659b4251b9160
alanrobg <81394144+alanrobg@users.noreply.github.com>
2023-07-05 21:54:02 -0500

    test

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>T</title>
+        <title>TEST</title>
     </head>
     <body>
         <?php
