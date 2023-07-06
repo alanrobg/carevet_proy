df3f914acc6df0505405fc4676546b71bfa78a21
DGSLP1937 <116422582+DGSLP1937@users.noreply.github.com>
2023-07-06 16:57:17 -0500

    Update bd_veterinaria.sql

--- bd_veterinaria.sql
+++ bd_veterinaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-07-2023 a las 05:07:37
+-- Tiempo de generación: 06-07-2023 a las 23:56:48
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -76,6 +76,25 @@ INSERT INTO `especie` (`idespecie`, `nom_especie`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `esterilizacion`
+--
+
+CREATE TABLE `esterilizacion` (
+  `idesterilizacion` int(11) NOT NULL,
+  `nom_esterilizacion` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `esterilizacion`
+--
+
+INSERT INTO `esterilizacion` (`idesterilizacion`, `nom_esterilizacion`) VALUES
+(1, 'Esterilizado'),
+(2, 'No Esterilizado');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `mascota`
 --
 
@@ -87,9 +106,18 @@ CREATE TABLE `mascota` (
   `color_mascota` varchar(30) NOT NULL,
   `registro_mascota` datetime NOT NULL,
   `foto_mascota` text NOT NULL,
-  `esterilazado_mascota` varchar(5) NOT NULL,
-  `id_raza` int(11) NOT NULL
+  `esterilizado` int(1) NOT NULL,
+  `idraza` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `mascota`
+--
+
+INSERT INTO `mascota` (`idmascota`, `nom_mascota`, `idcliente`, `nacimiento_mascota`, `color_mascota`, `registro_mascota`, `foto_mascota`, `esterilizado`, `idraza`) VALUES
+(1, 'Alan', 1, '2000-07-18', 'Blancon1', '2023-07-06 09:43:48', '/xampp/htdocs/carevet_proy/Vista/Mascotas_Fotos/Alan-Marlen_Obono Arpal_12312322/10-48-12_06-07-2023.png', 1, 1),
+(2, 'Jonathan', 2, '2023-07-04', 'Blanco', '2023-07-06 10:50:54', '/xampp/htdocs/carevet_proy/Vista/Mascotas_Fotos/Jonathan-Robles GrilloB5_Alan RaulB2_123123123/05-50-54_06-07-2023.mp4', 2, 1),
+(3, 'Prueba B4', 4, '2023-01-03', 'Negro', '2023-07-06 15:36:25', '/xampp/htdocs/carevet_proy/Vista/Mascotas_Fotos/Prueba B4-JonathanV2_Reyna V2_12415115/11-16-22_06-07-2023.mp4', 2, 41);
 
 -- --------------------------------------------------------
 
@@ -134,6 +162,14 @@ CREATE TABLE `sesion` (
   `fexpiracion_ses` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `sesion`
+--
+
+INSERT INTO `sesion` (`idsesion`, `key_ses`, `idusuario`, `fexpiracion_ses`) VALUES
+(28, '7029df142294110e7588e33bb403222eaf406c89497a0e327ea5e1023bda4a87', 1, '2023-07-06 11:17:29'),
+(29, 'ac051fee29306c0e0459a27b5ceb6d419dad46544700afd494b2c6d285fa979a', 1, '2023-07-06 13:55:33');
+
 -- --------------------------------------------------------
 
 --
@@ -160,7 +196,8 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`idusuario`, `ape_usuario`, `nom_usuario`, `dni_usuario`, `direccion_usuario`, `nacimiento_usuario`, `telefono_usuario`, `correo_usuario`, `contrato_usuario`, `idarea`, `usu_usuario`, `pass_usuario`) VALUES
-(1, 'Admin', 'Admin', '88888888', 'Lima', '1990-01-01', '123123123', 'test@test.com', 'TP', 1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
+(1, 'Admin', 'Admin', '88888888', 'Lima', '1990-01-01', '123123123', 'test@test.com', 'TP', 1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'),
+(2, 'Robles', 'Alan', '123124', 'Lima', '1990-01-01', '1512341', 'prueb@gmail.com', 'TP', 1, 'admin2', '1c142b2d01aa34e9a36bde480645a57fd69e14155dacfab5a3f9257b77fdc8d8');
 
 -- --------------------------------------------------------
 
@@ -197,6 +234,12 @@ ALTER TABLE `cliente`
 --
 ALTER TABLE `especie`
   ADD PRIMARY KEY (`idespecie`);
+
+--
+-- Indices de la tabla `esterilizacion`
+--
+ALTER TABLE `esterilizacion`
+  ADD PRIMARY KEY (`idesterilizacion`);
 
 --
 -- Indices de la tabla `mascota`
@@ -245,10 +288,16 @@ ALTER TABLE `especie`
   MODIFY `idespecie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT de la tabla `esterilizacion`
+--
+ALTER TABLE `esterilizacion`
+  MODIFY `idesterilizacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de la tabla `mascota`
 --
 ALTER TABLE `mascota`
-  MODIFY `idmascota` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idmascota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `raza`
@@ -260,13 +309,13 @@ ALTER TABLE `raza`
 -- AUTO_INCREMENT de la tabla `sesion`
 --
 ALTER TABLE `sesion`
-  MODIFY `idsesion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `idsesion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario_area`
